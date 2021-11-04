46b596d4f6e3243226ce0ad18c6c1770153dd711
TheKacho <abyssknightzero@gmail.com>
2021-11-03 22:55:54 -0400

    commented out Slick and implemented several lines of JS to do a similar carousel feature, the only thing that needs to be fixed is the image sizes themselves, going to need to crop/resize them to a consistent width. I do want to focus on making a hamburger bar for the nav on mobile.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 //This part of the script uses Slick to convert the images into a horizontal carousel gallery with 
-$('.gallery').slick();
+//$('.gallery').slick();
 
 // note that this will enable the previous and next buttons, as when the user clicks on either button, it will switch over to the 
 // next image. Swiping does work too
+
+
+var images = document.getElementsByClassName('img-responsive');
+var currentImageIndex = 0;
+UpdateImages();
+
+function IncrementGallery(){
+    if(currentImageIndex == images.length - 1){
+        currentImageIndex = 0;
+    }
+    else currentImageIndex++;
+    UpdateImages();
+}
+
+function DecrementGallery(){
+    if(currentImageIndex == 0){
+        currentImageIndex = images.length - 1;
+    }
+    else currentImageIndex--;
+    UpdateImages();
+}
+
+function UpdateImages(){
+    for(let i = 0; i < images.length; i++){
+        images[i].style.display = 'none';
+    }
+    images[currentImageIndex].style.display = 'block';
+}
