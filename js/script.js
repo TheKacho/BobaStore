b680e05ed267ba60f3e79cd448434ee9b74be4a3
TheKacho <abyssknightzero@gmail.com>
2021-11-03 20:19:31 -0400

    I have put all of the html script tags near the bottom of the body. still not sure if i need jquery, but will decide later.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-    $('.your-class').slick({
-        setting: setting - value
-});
-});
+//This part of the script uses Slick to convert the images into a horizontal carousel gallery with 
+$('.gallery').slick();
 
+// note that this will enable the previous and next buttons, as when the user clicks on either button, it will switch over to the 
+// next image. Swiping does work too
